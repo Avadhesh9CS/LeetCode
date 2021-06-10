d591420f1218041dff8d3135fe5388a0a828db2a
Avadhesh Chamola <84912436+Avadhesh9CS@users.noreply.github.com>
2021-06-10 20:29:02 +0530

    Add files via upload

--- 145_Binary_Tree_Postorder_Traversal.cpp
+++ 145_Binary_Tree_Postorder_Traversal.cpp
@@ -53,3 +53,67 @@ public:
         return output;
     }
 };
+
+//Solution 2->Morris Traversal
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> output;
+        TreeNode* dummy=new TreeNode(-1);
+        dummy->left=root;
+        TreeNode* curr= dummy;
+        TreeNode* pre=NULL;
+        TreeNode* prev=NULL;
+        TreeNode* succ=NULL;
+        TreeNode* temp=NULL;
+        while(curr){
+            if(curr->left){
+                pre=curr->left;
+                while(pre->right && pre->right!=curr){
+                    pre=pre->right;
+                }
+                if(!pre->right){
+                    pre->right=curr;
+                    curr=curr->left;
+                }else{
+                    pre->right=NULL;
+                    succ=curr;
+                    curr=curr->left;
+                    prev=NULL;
+                    
+                    while(curr){
+                        temp=curr->right;
+                        curr->right=prev;
+                        prev=curr;
+                        curr=temp;
+                    }
+                    
+                    while(prev){
+                        output.push_back(prev->val);
+                        temp=prev->right;
+                        prev->right=curr;
+                        curr=prev;
+                        prev=temp;
+                    }
+                    curr=succ;
+                    curr=curr->right;
+                }
+                
+            }else{
+                curr=curr->right;
+            }    
+        }
+     return output;   
+    }
+};
