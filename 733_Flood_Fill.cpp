c398dfaeba87ea567d246ba53553bb3c85499e00
Avadhesh Chamola <84912436+Avadhesh9CS@users.noreply.github.com>
2021-06-20 09:38:43 +0530

    Update 733_Flood_Fill.cpp

--- 733_Flood_Fill.cpp
+++ 733_Flood_Fill.cpp
@@ -23,3 +23,40 @@ Constraints:
     0 <= sr < m
     0 <= sc < n
 */
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        vector<vector<int>> visited(image.size(),vector<int>(image[0].size(),0));
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        visited[sr][sc]=1;
+        int i,j;
+        while(!q.empty()){
+            i=q.front().first;
+            j=q.front().second;
+            q.pop();
+            //top
+            if(i-1>=0 && visited[i-1][j]==0 && image[i][j]==image[i-1][j]){
+                visited[i-1][j]=1;
+                q.push({i-1,j});
+            }
+            //bottom
+            if(i+1<image.size() && visited[i+1][j]==0 && image[i][j]==image[i+1][j]){
+                visited[i+1][j]=1;
+                q.push({i+1,j});
+            }
+            //left
+            if(j-1>=0 && visited[i][j-1]==0 && image[i][j]==image[i][j-1]){
+                visited[i][j-1]=1;
+                q.push({i,j-1});
+            }
+            //right
+            if(j+1<image[0].size() && visited[i][j+1]==0 && image[i][j]==image[i][j+1]){
+                visited[i][j+1]=1;
+                q.push({i,j+1});
+            }
+            image[i][j]=newColor;
+        }
+        return image;
+    }
+};
