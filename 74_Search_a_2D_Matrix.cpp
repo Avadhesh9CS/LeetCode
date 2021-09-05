bec26f6be5a1eddc970ff4bc033a0bfe0de5248f
Avadhesh Chamola <84912436+AvadheshChamola@users.noreply.github.com>
2021-09-06 02:03:13 +0530

    Add files via upload

---
+++ 74_Search_a_2D_Matrix.cpp
@@ -0,0 +1,62 @@
+/*
+74. Search a 2D Matrix
+Medium
+
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+    Integers in each row are sorted from left to right.
+    The first integer of each row is greater than the last integer of the previous row.
+
+ 
+
+Example 1:
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+
+Example 2:
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+
+ 
+
+Constraints:
+
+    m == matrix.length
+    n == matrix[i].length
+    1 <= m, n <= 100
+    -104 <= matrix[i][j], target <= 104
+
+*/
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size(),n=matrix[0].size(),row=0;
+        int l=0,h=m-1,mid;
+        while(l<=h){
+            mid=l+((h-l)>>1);
+            if(matrix[mid][n-1]==target) return true;
+            if((mid==0 || matrix[mid-1][n-1]<target) && matrix[mid][n-1]>target){
+                row=mid;
+                break;
+            }else if(matrix[mid][n-1]<target){
+                l=mid+1;
+            }else{
+                h=mid-1;
+            }
+        }
+        l=0,h=n-1;
+        while(l<=h){
+            mid=l+((h-l)>>1);
+            if(matrix[row][mid]==target){
+                return true;
+            }else if(matrix[row][mid]<target){
+                l=mid+1;
+            }else{
+                h=mid-1;
+            }
+        }
+        return false;
+    }
+};
